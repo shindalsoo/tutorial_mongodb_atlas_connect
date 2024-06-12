6422fe82fbee3731b16516cdb351973afcce0f12
shindalsoo <shindalsoo@naver.com>
2024-06-13 00:49:20 +0900

    비밀번호 암호화 적용

--- config.js
+++
@@ -1,4 +0,0 @@
-const config = {
-    mongodbUri: 'mongodb+srv://shindalsoo:sds9050%40%23!@zioyou.zkjwmfy.mongodb.net/myFirstDatabase?authSource=admin',
-}
-export default config; 
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
 import path from "path";
-import config from "./config.js";
 import routes from "./routes/routes.js";
+import dotenv from "dotenv";
 
+dotenv.config();
 console.log(`ecode password:${encodeURIComponent("sds9050@#!")}`);
+
 mongoose.set("strictQuery", true); // 스키마에서 정의한 필드만 받는다.
 mongoose
   .connect(
